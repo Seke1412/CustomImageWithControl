b3b7d8501007b9faaedd4a3e09bc49657d32432b
Ngô Dũng Cần <ngodungcan@gmail.com>
2016-02-24 01:57:13 +0700

    image success load from local storage. Main class finish checking parameters valid

--- js/common.js
+++ js/common.js
@@ -3,6 +3,57 @@ $(function(){
     trace("document successfully init");
 
 
+    var inputBtn = $("#file-input"),
+        currentRot = 0,
+        currentX = 0,
+        currentY = 0,
+        currentSX = 1,
+        currentSY = 1,
+        editImage = $("#editIMG"),
+        alert = $(".alert"),
+        currentFile,
+        init = function(){
+            inputBtn.on("change", onSelectImageComplete);
+        },
+        onSelectImageComplete = function(e){
+            e.preventDefault();
+            var target = e.target,
+                file = target && target.files && target.files[0],
+                options = {
+                    //maxWidth: originalImage.width(),
+                    canvas: true
+                };
+            if (!file) {
+                return;
+            }
+            loadImage.parseMetaData(file, function (data) {
+                if (data.exif) {
+                    options.orientation = data.exif.get('Orientation');
+                    //displayExifData(data.exif);
+                }
+                doLoadImage(file, options);
+            });
+        },
+        doLoadImage = function(_file, _options){
+            trace("doLoadedImage trigger success");
+            currentFile = _file;
+            if (!loadImage(_file, showLoadedImage, _options)) {
+                alert.html("Your browser does not support the URL or FileReader API.");
+            }
+        },
+        showLoadedImage = function(img){
+            var originalImage = document.getElementById("originalIMG");
+            trace("showLoadedImage trigger success");
+            if (!(img.src || img instanceof HTMLCanvasElement)) {
+                alert.html("Loading image file failed");
+            } else {
+                trace(img.toDataURL());
+                originalImage.src = img.toDataURL();
+            }
+
+        };
+    init();
+
 })
 
 var trace = function(s){
--- js/custom-image-with-control.js
+++ js/custom-image-with-control.js
@@ -1,6 +1,29 @@
 (function($){
     'use strict'
+    var editImage = function(_img, _options, _callback){
+        var img, options, callback;
 
+        //Check _img, _options, _callback validation
+        // if _img is not valid return and exit;
+        if (img == null || img == undefined){
+            return {
+                error:1,
+                mess: "no image found"
+            };
+        }else{
+            img = img;
+        }
+        //if user doesn't input _options, we will use a default options;
+        if (_options == null || _options == undefined){
+            options = {};
+        }
 
+        // if user doesn't provide callback => marked it as "none" we are free;
+        if(_callback == null || _callback == undefined){
+            callback = "none";
+        }else{
+            callback = _callback;
+        }
+    };
 
 }(window));
